5db75072b2ca4659c57ccdf3c0529b9f957f3774
Laura Badgley <l.badgleyy@gmail.com>
2019-09-16 16:28:55 -0700

    changing to SeasonDisplay

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = {
-      
-    };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude, long: position.coords.longitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+    console.log('My component was rendered to the screen');
   }
 
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!');
+  }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      (err) => console.log(err)
-    )
-    return <div>Latitude: </div>
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>{this.state.errorMessage}</div>
+    } 
+
+    if(!this.state.errorMessage && this.state.lat) {
+      return <div><div>Latitude: {this.state.lat}</div><div> Longitude: {this.state.long}</div></div>
+    }
+
+    return <div>Loading!</div>
   }
 }
 ReactDOM.render( 
